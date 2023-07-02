ed4a71f5d4d7dd5fda620c6ed483b3933b26c879
Fer <fernanda23demarzo@gmail.com>
2023-07-02 19:08:21 -0300

    Agregué a carpetas

---
+++ src/components/Inicio/Inicio.jsx
@@ -0,0 +1,20 @@
+import './Inicio.css';
+
+
+export const Inicio = () => {
+    return (
+
+          <section className='inicio' id='inicio'>
+
+          <div>
+            <h2 className="hola">Hola<br/>Soy Fernanda,</h2>
+            <h1 className="titulo">Jr. Front-End Developer.</h1>
+          </div>
+
+          <div className='img'>
+            <img src="/img/avatar.png"/>
+          </div>
+          </section>
+
+    );
+}
