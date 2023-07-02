2610344a37e7b1f4ab822b2b0f746418a307ca75
Fer <fernanda23demarzo@gmail.com>
2023-07-02 19:08:45 -0300

    Arreglé rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { NavBar } from './components/NavBar';
-import { Inicio } from './components/Inicio';
+import { NavBar } from './components/NavBar/NavBar';
+import { Inicio } from './components/Inicio/Inicio';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { SobreMi } from './components/SobreMi';
+import { SobreMi } from './components/SobreMi/SobreMi';
 
 function App() {
 
