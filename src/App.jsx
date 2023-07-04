99e1c3c6a0c11a281de510141edf8f36fdde752b
Fer <fernanda23demarzo@gmail.com>
2023-07-04 20:42:56 -0300

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { NavBar } from './components/NavBar/NavBar';
 import { Inicio } from './components/Inicio/Inicio';
 import { SobreMi } from './components/SobreMi/SobreMi';
 import { Skills } from './components/Skills/Skills';
+import { Proyectos } from './components/Proyectos/Proyectos';
+import { Contacto } from './components/Contacto/Contacto';
 import './App.css'
+
 
 
 
@@ -15,6 +18,8 @@ function App() {
         <Inicio/>
         <SobreMi/>
         <Skills/>
+        <Proyectos/>
+        <Contacto/>
     </>
   )
 }
